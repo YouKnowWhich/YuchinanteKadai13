af54d5b4cda51ba8e6862165c81121b98f3d7a2f
yuya-3110 <ysuu45143745@gmail.com>
2024-04-11 02:13:21 +0900

    first commit

---
+++ YuchinanteKadai13/TableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TableViewCell.swift
+//  YuchinanteKadai13
+//  
+//  Created by Yuchinante on 2024/04/10
+//  
+//
+
+import UIKit
+
+class TableViewCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
